f178484651d503dbc9ce1d2e27e8742e88c52892
mahad-prog <73501069+mahad-prog@users.noreply.github.com>
2020-11-28 01:28:12 -0500

    new

---
+++ FinalProtoypeI/Assets/All Scripts/ToolsDR.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolsDR : MonoBehaviour
+{
+    
+    public void ToggleActive () {
+ 
+	gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
