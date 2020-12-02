5b3ce8c024038bd6e0e382291f33815654f1dddd
SarahQadri <73500531+SarahQadri@users.noreply.github.com>
2020-12-02 12:11:29 -0500

    new

---
+++ FinalProtoypeI/Assets/All Scripts/Gaze.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Gaze : MonoBehaviour
+{
+    List<InfoBehavior> infos = new List<InfoBehavior>();
+
+    void Start()
+    {
+        infos = FindObjectsOfType<InfoBehavior>().ToList();
+    }
+
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+        {
+            GameObject go = hit.collider.gameObject;
+            if (go.CompareTag("hasInfo"))
+            {
+                print("HERE");
+            }
+        }
+    }
+}
